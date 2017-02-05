7ab628449b6342b77d2923885176f326f341af78
tiagomartinho <tiago.martinho@xpeppers.com>
2017-02-05 18:00:44 +0100

    Add nameTextField and numberTextField outlet

--- RxSpike/AddContactViewController.swift
+++ RxSpike/AddContactViewController.swift
@@ -1,6 +1,11 @@
 import UIKit
+import RxCocoa
+import RxSwift
 
 class AddContactsViewController: UITableViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
 
     @IBAction func save(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -8,5 +13,22 @@ class AddContactsViewController: UITableViewController {
 
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let nameValidator = nameTextField.rx.text.orEmpty
+            .distinctUntilChanged()
+            .flatMapLatest { query -> Observable<String> in
+                if query.isEmpty {
+                    return .just("Name is empty")
+                }
+
+                return .just("")
+            }
+            .observeOn(MainScheduler.instance)
+
+        tableView.rx.obe
     }
 }
