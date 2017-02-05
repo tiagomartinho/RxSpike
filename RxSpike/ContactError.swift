8526f7b669f33c2a515ea5e8e5fb426f91cd3c3c
Tiago Martinho <tiago@elit.software>
2017-02-05 19:33:00 +0100

    Adds full message to numberHasInvalidFormat message

--- RxSpike/ContactError.swift
+++ RxSpike/ContactError.swift
@@ -1,7 +1,7 @@
 enum ContactError: String {
     case emptyName = "Name is empty"
     case emptyNumber = "Number is empty"
-    case numberHasInvalidFormat = "Number should be a \"+\" followed by a nonempty group of digits"
+    case numberHasInvalidFormat = "Number should be a \"+\" followed by a nonempty group of digits, a space, a nonempty group of digits, a space, a group of digits with at least 6 digits"
 }
 
 extension Collection where Iterator.Element == ContactError {
