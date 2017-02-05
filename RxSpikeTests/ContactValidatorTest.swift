b379ac0abda3970c84473f43879b2144885b74cf
Tiago Martinho <tiago@elit.software>
2017-02-05 18:25:03 +0100

    Empty name contact return emptyName error

--- RxSpikeTests/ContactValidatorTest.swift
+++ RxSpikeTests/ContactValidatorTest.swift
@@ -3,14 +3,44 @@ import XCTest
 class ContactValidatorTest: XCTestCase {
     
     func testEmptyContactIsInvalid() {
-        let contact = Contact()
+        let contact = Contact(name: "", number: "")
 
         let isValid = contact.isValid
 
         XCTAssertFalse(isValid)
     }
+
+    func testEmptyNameReturnNameEmptyError() {
+        let contact = Contact(name: "", number: "")
+
+        let errors = contact.errors
+
+        XCTAssert(errors.contains(.emptyName))
+    }
 }
 
 struct Contact {
-    var isValid = false
+
+    let name: String
+    let number: String
+
+    init(name: String, number: String) {
+        self.name = name
+        self.number = number
+    }
+}
+
+extension Contact {
+
+    var errors: [ContactError] {
+        return [.emptyName]
+    }
+
+    var isValid: Bool {
+        return errors.isEmpty
+    }
+}
+
+enum ContactError {
+    case emptyName
 }
